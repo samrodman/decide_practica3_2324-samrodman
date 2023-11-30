310c5e9e89ce0fbf6a5d9ae1da7282d09b8d9737
Sammy <samrodman@alum.us.es>
2023-11-30 09:09:15 +0100

    changes

--- vagrant/files/settings.py
+++ vagrant/files/settings.py
@@ -1,43 +1,52 @@
 DEBUG = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
-        'USER': 'decide',
-        'PASSWORD': 'decide',
-        'HOST': 'localhost',
-        'PORT': 5432,
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "decide",
+        "USER": "decide",
+        "PASSWORD": "decide",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
-STATIC_ROOT = '/home/decide/static/'
-MEDIA_ROOT = '/home/decide/static/media/'
-ALLOWED_HOSTS = ['*']
+STATIC_ROOT = "/home/decide/static/"
+MEDIA_ROOT = "/home/decide/static/media/"
+ALLOWED_HOSTS = ["*"]
+ALLOWED_ORIGINS = [
+    "http://*",
+    "https://*",
+    "https://localhost:8080",
+    "http://localhost:8080",
+]
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = "Strict"
+CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS.copy()
 
 # Modules in use, commented modules that you won't use
 MODULES = [
-    'authentication',
-    'base',
-    'booth',
-    'census',
-    'mixnet',
-    'postproc',
-    'store',
-    'visualizer',
-    'voting',
+    "authentication",
+    "base",
+    "booth",
+    "census",
+    "mixnet",
+    "postproc",
+    "store",
+    "visualizer",
+    "voting",
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = "http://localhost:8000"
 
 APIS = {
-    'authentication': 'http://localhost:8000',
-    'base': 'http://localhost:8000',
-    'booth': 'http://localhost:8000',
-    'census': 'http://localhost:8000',
-    'mixnet': 'http://localhost:8000',
-    'postproc': 'http://localhost:8000',
-    'store': 'http://localhost:8000',
-    'visualizer': 'http://localhost:8000',
-    'voting': 'http://localhost:8000',
+    "authentication": "http://localhost:8000",
+    "base": "http://localhost:8000",
+    "booth": "http://localhost:8000",
+    "census": "http://localhost:8000",
+    "mixnet": "http://localhost:8000",
+    "postproc": "http://localhost:8000",
+    "store": "http://localhost:8000",
+    "visualizer": "http://localhost:8000",
+    "voting": "http://localhost:8000",
 }
